84d3d755d161adf98ea5eb38d9431dc80ed4d8b5
Harshroc <93042806+Harshroc@users.noreply.github.com>
2022-01-02 04:01:23 +0530

    updated

--- api/controller/products.js
+++ api/controller/products.js
@@ -12,9 +12,9 @@ exports.get_products = (req, res, next) => {
 exports.get_product = (req, res, next) => {
   const prod = req.params.id;
   productsModel.find({'_id': prod}).populate('productCategory','categoryName').exec().then((product) =>{
-      res.status(200).json(product);
+      res.status(200).json({product, message: "Success"});
     }).catch((error) => {
-      res.status(500).send(error);
+      res.status(500).json({error: error});
     });
 };
 
